7c6abd3afe7da35c27b8f9bbe1d12bdf19c73d42
Ulas Akce <109419860+ulasakce@users.noreply.github.com>
2026-01-29 06:50:40 +0300

    Change

--- index.tsx
+++ index.tsx
@@ -3,14 +3,52 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+/**
+ * Wix Studio Custom Element Desteği
+ */
+class RedPixelElement extends HTMLElement {
+  private root: ReactDOM.Root | null = null;
+
+  connectedCallback() {
+    if (!this.root) {
+      this.root = ReactDOM.createRoot(this);
+    }
+    this.render();
+  }
+
+  render() {
+    if (this.root) {
+      this.root.render(
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      );
+    }
+  }
+
+  disconnectedCallback() {
+    if (this.root) {
+      this.root.unmount();
+      this.root = null;
+    }
+  }
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Custom Element'i tanımla
+if (typeof window !== 'undefined' && !customElements.get('red-pixel-app')) {
+  customElements.define('red-pixel-app', RedPixelElement);
+}
+
+/**
+ * Standart Root Mount (Vercel Önizleme için)
+ * index.html içindeki id="root" olan div'i hedefler.
+ */
+const rootElement = document.getElementById('root');
+if (rootElement && !rootElement.hasChildNodes()) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
